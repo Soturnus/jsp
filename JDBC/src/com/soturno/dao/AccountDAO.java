89335e8b63d97a3a564ba12444eb3257a9707f4b
Soturnus <rivaldo.valhalla@gmail.com>
2023-09-24 02:47:12 -0300

    leitura de dados resultSet

--- JDBC/src/com/soturno/dao/AccountDAO.java
+++ JDBC/src/com/soturno/dao/AccountDAO.java
@@ -2,6 +2,7 @@ package com.soturno.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -17,16 +18,28 @@ public class AccountDAO {
 			
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", user, password);
 			Statement statement = conn.createStatement();
+			
 			/** insert */
 			//int result = statement.executeUpdate("insert into account values(1, 'thippireddy', 'soturno', 10000)");
 			/** update */
 			//int result = statement.executeUpdate("update account set bal=65466 where accno=1");
-			/** update */
-			int result = statement.executeUpdate("delete from account where accno=1");
+			/** delete */
+			//int result = statement.executeUpdate("delete from account where accno=1");
+					
+			//System.out.println(result + " linhas afetadas");
 			
-			System.out.println(result + " linhas afetadas");
+			/** ler dados do banco */
+			ResultSet rs = statement.executeQuery("select * from account;");
+					
+			while(rs.next()) {
+				System.out.println("Last Name: " + rs.getString(2));
+				System.out.println("First Name: " + rs.getString(3));
+				System.out.println("Balance: " + rs.getInt(4));	
+			}
+			/** fim da leitura */
+			
 		} catch (SQLException e) {
-			e.getMessage();
+			System.out.println(e.getMessage());
 		}
 	}
 }
